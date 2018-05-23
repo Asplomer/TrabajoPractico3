5a7a2b2a43051c2141da2e6de50ad81610da271d
stranker <dani_mrx93@hotmail.com>
2018-05-23 11:45:40 -0300

    Se agrega posicion de Mario y el input

--- RPGMaster/Main.cpp
+++ RPGMaster/Main.cpp
@@ -1,17 +1,34 @@
-#include "allegro5/allegro.h"
+#include <stdio.h>
+#include <allegro5/allegro.h>
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_native_dialog.h"
 
-int main(int argc, char **argv) {
+const float FPS = 60;
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+const int BOUNCER_SIZE = 32;
 
+int main(int argc, char **argv)
+{
 	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_BITMAP  *image = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *mario = NULL;
+	ALLEGRO_BITMAP *enemy = NULL;
+	float positionX = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+	float positionY = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+	float marioW = 128;
+	float marioH = 128;
+	float enemyPositionX = 0;
+	float enemyPositionY = 0;
+	float enemyW = 128;
+	float enemyH = 128;
+	bool redraw = true;
 	bool gameOver = false;
 
 	if (!al_init()) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return 0;
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
 	}
 
 	if (!al_init_image_addon()) {
@@ -20,33 +37,106 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	display = al_create_display(800, 600);
+	if (!al_install_keyboard())
+	{
+		fprintf(stderr, "failed to initialize the Keyboard!\n");
+		return -1;
+	}
 
+	timer = al_create_timer(1.0 / FPS);
+	if (!timer) {
+		fprintf(stderr, "failed to create timer!\n");
+		return -1;
+	}
+
+	display = al_create_display(SCREEN_W, SCREEN_H);
 	if (!display) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return 0;
+		fprintf(stderr, "failed to create display!\n");
+		al_destroy_timer(timer);
+		return -1;
 	}
 
-	image = al_load_bitmap("Asset/Mario.png");
-
-	if (!image) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+	mario = al_load_bitmap("Asset/Mario.png");
+	if (!mario) {
+		fprintf(stderr, "failed to create bouncer bitmap!\n");
 		al_destroy_display(display);
-		return 0;
+		al_destroy_timer(timer);
+		return -1;
 	}
+
+	enemy = al_load_bitmap("Asset/Goomba.png");
+	if (!enemy) {
+		fprintf(stderr, "failed to create bouncer bitmap!\n");
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	al_set_target_bitmap(mario);
+	al_set_target_bitmap(enemy);
+
+	al_set_target_bitmap(al_get_backbuffer(display));
+
+	event_queue = al_create_event_queue();
+	if (!event_queue) {
+		fprintf(stderr, "failed to create event_queue!\n");
+		al_destroy_bitmap(mario);
+		al_destroy_bitmap(enemy);
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+
+	al_flip_display();
+
+	al_start_timer(timer);
+
 	while (!gameOver)
 	{
-		al_draw_bitmap(image, 200, 200, 0);
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
 
-		al_flip_display();
-		al_rest(2);
+		if (ev.type == ALLEGRO_EVENT_TIMER) {
+			redraw = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+			if (ev.keyboard.keycode == ALLEGRO_KEY_DOWN)
+				positionY += 10;
+			else if (ev.keyboard.keycode == ALLEGRO_KEY_UP)
+				positionY -= 10;
+			else if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT)
+				positionX -= 10;
+			else if (ev.keyboard.keycode == ALLEGRO_KEY_RIGHT)
+				positionX += 10;
+		}
 
-		al_destroy_display(display);
-		al_destroy_bitmap(image);
+		if (redraw && al_is_event_queue_empty(event_queue)) {
+			redraw = false;
+
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+
+			al_draw_bitmap(mario, positionX, positionY, 0);
+
+			al_flip_display();
+		}
 	}
 
+	al_destroy_bitmap(mario);
+	al_destroy_bitmap(enemy);
+	al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
 
 	return 0;
 }
